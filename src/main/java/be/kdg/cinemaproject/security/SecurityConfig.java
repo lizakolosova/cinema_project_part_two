16196c5d52eb0c9693275984c2bcee2aaf0cfbc8
lzkolosova <liza.kolosova@student.kdg.be>
2025-04-25 16:07:56 +0200

    Added enable method security annotation

--- src/main/java/be/kdg/cinemaproject/security/SecurityConfig.java
+++ src/main/java/be/kdg/cinemaproject/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import static org.springframework.security.web.util.matcher.RegexRequestMatcher.
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(final HttpSecurity security) throws Exception {
