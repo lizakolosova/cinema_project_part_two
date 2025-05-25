86ab2c01a6834ff942f498717e5038e6c6654469
lzkolosova <liza.kolosova@student.kdg.be>
2025-05-25 16:00:41 +0200

    Base before customization

---
+++ src/main/js/index.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/site.scss";
+import "../css/index.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
